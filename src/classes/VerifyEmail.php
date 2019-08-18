53c00a2fa433904faa9d10c682ba8fe1383c3cea
Matt Watson <matt@mattwatson.org>
2019-08-18 17:43:53 -0500

    Added verify email page

---
+++ src/classes/VerifyEmail.php
@@ -0,0 +1,51 @@
+<?php
+
+class VerifyEmail {
+	function show($f3, $args) {
+		Utils::redirect_logged_out_user($f3, $args);
+        Utils::send_csrf($f3, $args);
+        $f3->set('v_page_title', 'Verify Email');
+
+        // GET DB, SESSION AND USER
+		$db = $f3->get('DB');
+		$session_username = $f3->get('SESSION.session_username');
+		$db_users = new \DB\SQL\Mapper($db, 'users');
+		$user = $db_users->load(array('username=?', $session_username));
+        $f3->set('v_username', $session_username);
+        $f3->set('v_email_verified', $user->email_verified === 1 ? true : false);
+        $f3->set('v_user_email', $user->email ? : 'no email address on record');
+        // RENDER
+		$view = new \View;
+        echo $view->render('verify-email.php');
+    }
+
+    function verify_email($f3, $args) {
+        Utils::redirect_logged_out_user($f3, $args);
+        Utils::prevent_csrf_from_tab_conflict($f3, $args, '/verify-login');
+        
+        $req = $f3->get('REQUEST');
+        $req_code = $req['email_verification_code'];
+        
+        // GET DB, SESSION AND USER
+		$db = $f3->get('DB');
+		$session_username = $f3->get('SESSION.session_username');
+		$db_users = new \DB\SQL\Mapper($db, 'users');
+        $user = $db_users->load(array('username=?', $session_username));
+        
+        if(password_verify($req_code, $user->email_verification_hash)) {
+            $user->email_verified = 1;
+            $user->save();
+            $f3->push('v_confirmations', array(
+				'element_id' => 'verify_email_confirmations',
+				'message' => 'You have successfully verified your email address, '.$user->email.'!'
+			));
+			Utils::reroute_with_confirmations($f3, $args, '/verify-email');
+		} else {
+			$f3->push('v_errors', array(
+				'element_id' => 'verify_email_errors',
+				'message' => 'Email verification failed.'
+			));
+			Utils::reroute_with_errors($f3, $args, '/verify-email');
+		}
+    }
+}
---
+++ templates/verify-email.php
@@ -0,0 +1,58 @@
+<?php require_once('partials/header.php'); ?>
+
+<div class="container mb-5">
+	<div class="row">
+		<?php require_once('partials/heading.php'); ?>
+		<?php require_once('partials/account.php'); ?>
+	</div>
+</div>
+
+
+<div class="container mb-5">
+	<div class="row">
+		<div class="col-lg-6">
+			<?php if(count($v_confirmations)) : ?>
+			<div class="alert alert-success" role="alert">
+				<?php
+				foreach($v_confirmations as $confirmation) :
+				?>
+				<p><?php echo $confirmation->message; ?></p>
+				<?php endforeach; ?>
+			</div>
+			<?php endif; ?>
+
+			<?php if(in_array(
+				'verify_email_errors', $v_errors_element_ids ? : array()
+			)) : ?>
+			<div class="alert alert-danger" role="alert">
+				<?php
+				foreach($v_errors as $error) :
+				if($error->element_id === 'verify_email_errors') :
+				?>
+				<p><?php echo $error->message; ?></p>
+				<?php endif; endforeach; ?>
+			</div>
+			<?php endif; ?>
+
+            <?php if(!$v_email_verified): ?>
+			<form action="/verify-email" method="POST">
+				<input type="text" name="csrf" id="csrf_stop_timer" value="<?php echo $CSRF; ?>" hidden>
+				
+                <h2>Verify Email</h2>
+                
+				<div class="form-group">
+					<label for="email_verification_code">Enter in the email verification code you received.</label>
+					<input class="form-control"type="text" name="email_verification_code" id="email_verification_code">
+                </div>
+                
+                <div class="form-group">
+					<input type="submit" value="Submit" class="btn btn-primary">
+				</div>
+            </form>
+            <?php else: ?>
+            <p>Your email address <strong><?php echo $v_user_email; ?></strong> has already been verified.</p>
+            <?php endif; ?>
+		</div>
+	</div>
+</div>
+<?php require_once('partials/footer.php'); ?>
