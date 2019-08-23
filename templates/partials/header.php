b9d720ec56cd48b044c987a0a737b3bfb9f5946c
Matt Watson <matt@mattwatson.org>
2019-08-22 19:13:02 -0500

    Added noindex no follow to all pages

--- templates/partials/header.php
+++ templates/partials/header.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="robots" content="noindex, nofollow">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title><?php echo $SITE_NAME; ?> - <?php echo $v_page_title; ?></title>
 	<link rel="stylesheet" href="/dist/app.css">
