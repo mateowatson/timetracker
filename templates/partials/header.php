1710adadaefbd743ed4fd095a3ce553fc47d7dcb
Matt Watson <matt@mattwatson.org>
2019-08-24 11:38:45 -0500

    Added favicon

--- templates/partials/header.php
+++ templates/partials/header.php
@@ -7,6 +7,16 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title><?php echo $SITE_NAME; ?> - <?php echo $v_page_title; ?></title>
 	<link rel="stylesheet" href="/dist/app.css">
+
+
+
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="msapplication-TileColor" content="#1b335f">
+	<meta name="theme-color" content="#e4e4e4">
 </head>
 <body class="my-5">
 
