e53043edf01a193cc95c50b0cbcd79550923241f
Matt Watson <matt@mattwatson.org>
2019-08-31 23:22:43 -0500

    Fixed pagination bug

--- protected/src/classes/Report.php
+++ protected/src/classes/Report.php
@@ -176,14 +176,14 @@ class Report {
 			$prev_link = null;
 		} else {
 			$prev_link = "/report?rp=".$report_project."&rt=".
-				$report_task."&page=".(string)($page-1);
+				$report_task."&rd=".$report_date."&page=".(string)($page-1);
 		}
 
 		if($page+1 > $logs_count/10 || $logs_count <= 10) {
 			$next_link = null;
 		} else {
 			$next_link = "/report?rp=".$report_project."&rt=".
-				$report_task."&page=".(string)($page+1);
+				$report_task."&rd=".$report_date."&page=".(string)($page+1);
 		}
 		$f3->set('v_next_link', $next_link);
 		$f3->set('v_prev_link', $prev_link);
